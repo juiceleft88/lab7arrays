be8c5d70bd7cd0b2ccb639231acfd8e326a42749
juiceleft88 <juiceleft88@gmail.com>
2021-02-01 23:59:22 -0800

    changing 6 to num-plastics

--- hugoizquierdo_lab7.cpp
+++ hugoizquierdo_lab7.cpp
@@ -16,13 +16,13 @@ int main()
     const int NUM_PLASTICS = 6;
     string plastics[NUM_PLASTICS] = {"PET beverage bottles and jars", "Natural high-density polyethylene", "Color HDPE", "Grade A film", "Grade B film", "Grade C film"};
     double plasticPerLb[] = {.08, .34, .04, .08, .05, .02};
-    double numberOfLbs[6];
-    double totalCost[6];
+    double numberOfLbs[NUM_PLASTICS];
+    double totalCost[NUM_PLASTICS];
 
     cout << "Enter the weight for each item to create a purchase order." << endl;
 
     displayPlastics(plastics, NUM_PLASTICS); //to show plastics
-    
+    readNumLbs(numberOfLbs, 6);
 
 
     return 0;
@@ -30,20 +30,16 @@ int main()
 
 void displayPlastics(string plast[], int size)
 {   
-
+    double numberOfLbs[6];
     for (int i=0; i < size; i++)
+    cout << "Enter the amount of pounds for" << (i+1) << endl;
+    cin >> numberOfLbs[i];
     cout << plast[i] << endl;
 }
 
 void readNumLbs (double poundNum[], int size2)
 {
-    readNumLbs(numberOfLbs, 6);
     cout << "Please enter the amount of pounds for each plastic" << endl;
     for (int k = 0; k < size2; k++)
     cin >> poundNum[k];
-}
-
-double totalFunction (double);
-{
-    return total num2 again please
 }
